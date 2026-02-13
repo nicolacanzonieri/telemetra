7da2170e9c3eb0911f177ae1269ea6570542c1bb
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-02-13 19:33:43 +0100

    Added dexie and vite pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Telemetra',
+        short_name: 'Telemetra',
+        description: 'Telemetry for motorsport',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        orientation: 'landscape',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
   ],
 })
