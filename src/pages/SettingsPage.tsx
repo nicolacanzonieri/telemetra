f3e67e192658a4275ef4aa69d5b507d691c72691
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-02-14 16:08:18 +0100

    Add SettingsButton component for improved settings functionality

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -4,6 +4,30 @@ interface SettingsPageProps {
     onCloseSettings: () => void;
 }
 
+interface SettingsButtonProps {
+    type: 'action' | 'toggle';
+    label: string;
+}
+
+function SettingsButton({type, label}: SettingsButtonProps) {
+    return (
+        <>
+            {type == 'action' && (
+                <div className="w-full h-20 flex flex-row items-center justify-center p-5 border border-emerald-500">
+                    <span className="text-sm font-mono tracking-widest uppercase text-neutral-200">{label}</span>
+                </div>
+            )}
+
+            {type == 'toggle' && (
+                <div className="w-full h-20 flex flex-row items-center place-content-between p-5 border border-emerald-500">
+                    <span className="text-sm font-mono tracking-widest uppercase text-neutral-200">{label}</span>
+                    <span className="text-sm font-mono tracking-widest uppercase text-neutral-200">ON</span>
+                </div>
+            )}
+        </>
+    );
+}
+
 export default function SettingsPage({onCloseSettings}: SettingsPageProps) {
     return(
         <div className='w-screen h-screen absolute flex flex-col z-10 bg-black'>
@@ -11,7 +35,11 @@ export default function SettingsPage({onCloseSettings}: SettingsPageProps) {
                 <HeaderButton onClick={onCloseSettings} children={<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-x-icon lucide-x text-white active:text-neutral-400"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>}/>
             </div>
             <div className='flex flex-col flex-1 items-center justify-start p-5 gap-3 bg-black'>
-                <span className="text-white text-4xl font-bold italic uppercase">Settings</span>
+                <span className="mb-10 text-white text-4xl font-bold italic uppercase">Settings</span>
+
+                <SettingsButton type="toggle" label={"Dark mode"}/>
+                <SettingsButton type="action" label={"Update Telemetra"}/>
+                <SettingsButton type="action" label={"Clear all data and update"}/>
             </div>
         </div>
     );
