7d68b9124d132e4fd09ebdc868816470df6b8c99
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-02-14 19:55:35 +0100

    Refactor color variables in CSS and update component styles in SettingsPage and StartMenuPage for improved consistency and readability

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -13,13 +13,13 @@ function SettingsButton({type, label}: SettingsButtonProps) {
     return (
         <>
             {type == 'action' && (
-                <div className="w-full h-20 flex flex-row items-center justify-center p-5 border border-emerald-500 hover:bg-white/15 active:border-white active:bg-white/35">
+                <div className="w-full h-20 flex flex-row items-center justify-center p-5 border border-border-1-dark hover:bg-bg-hover-1-dark active:border-border-hover-1-dark active:bg-bg-active-1-dark">
                     <span className="text-sm font-mono tracking-widest uppercase text-neutral-200">{label}</span>
                 </div>
             )}
 
             {type == 'toggle' && (
-                <div className="w-full h-20 flex flex-row items-center place-content-between p-5 border border-emerald-500 hover:bg-white/15 active:border-white active:bg-white/35">
+                <div className="w-full h-20 flex flex-row items-center place-content-between p-5 border border-border-1-dark hover:bg-bg-hover-1-dark active:border-border-active-1-dark active:bg-bg-active-1-dark">
                     <span className="text-sm font-mono tracking-widest uppercase text-neutral-200">{label}</span>
                     <span className="text-sm font-mono tracking-widest uppercase text-neutral-200">ON</span>
                 </div>
@@ -32,10 +32,10 @@ export default function SettingsPage({onCloseSettings}: SettingsPageProps) {
     return(
         <div className='w-screen h-screen absolute flex flex-col z-10 bg-black'>
             <div className='h-20 flex flex-row items-center justify-end'>
-                <HeaderButton onClick={onCloseSettings} children={<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-x-icon lucide-x text-white active:text-neutral-400"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>}/>
+                <HeaderButton onClick={onCloseSettings} children={<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-x-icon lucide-x text-icon-1-dark active:text-icon-active-1-dark"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>}/>
             </div>
-            <div className='flex flex-col flex-1 items-center justify-start p-5 gap-3 bg-black'>
-                <span className="mb-10 text-white text-4xl font-bold font-mono tracking-widest uppercase">Settings</span>
+            <div className='flex flex-col flex-1 items-center justify-start p-5 gap-3 bg-bg-1-dark'>
+                <span className="mb-10 text-text-1-dark text-4xl font-mono tracking-widest uppercase">Settings</span>
 
                 <SettingsButton type="toggle" label={"Dark mode"}/>
                 <SettingsButton type="action" label={"Update Telemetra"}/>
--- src/pages/StartMenuPage.tsx
+++ src/pages/StartMenuPage.tsx
@@ -13,7 +13,7 @@ interface MenuButtonProps {
 
 function MenuButton({label, onClick}: MenuButtonProps) {
     return(
-        <button onClick={onClick} className='w-85 h-20 flex flex-row items-center justify-center bg-[rgb(0, 0, 0)] border border-emerald-500 font-mono tracking-widest uppercase text-neutral-200 hover:bg-white/15 active:border-white active:bg-white/35'>
+        <button onClick={onClick} className='w-85 h-20 flex flex-row items-center justify-center bg-[rgb(0, 0, 0)] border border-border-1-dark font-mono tracking-widest uppercase text-text-1-dark hover:bg-bg-hover-1-dark active:border-white active:bg-bg-active-1-dark'>
             {label}
         </button>
     );
@@ -32,12 +32,12 @@ export default function StartMenuPage({ onOpenSettings }: StartMenuPageProps) {
 
     return(
         <div className='w-screen h-screen absolute flex flex-col z-0'>
-            <div className='h-20 flex flex-row items-center justify-end'>
-                <HeaderButton onClick={onOpenSettings} children={<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-cog-icon lucide-cog text-white active:text-neutral-400"><path d="M11 10.27 7 3.34"/><path d="m11 13.73-4 6.93"/><path d="M12 22v-2"/><path d="M12 2v2"/><path d="M14 12h8"/><path d="m17 20.66-1-1.73"/><path d="m17 3.34-1 1.73"/><path d="M2 12h2"/><path d="m20.66 17-1.73-1"/><path d="m20.66 7-1.73 1"/><path d="m3.34 17 1.73-1"/><path d="m3.34 7 1.73 1"/><circle cx="12" cy="12" r="2"/><circle cx="12" cy="12" r="8"/></svg>}/>
+            <div className='h-20 flex flex-row items-center justify-end bg-bg-1-dark'>
+                <HeaderButton onClick={onOpenSettings} children={<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-cog-icon lucide-cog text-icon-1-dark active:text-icon-active-1-dark"><path d="M11 10.27 7 3.34"/><path d="m11 13.73-4 6.93"/><path d="M12 22v-2"/><path d="M12 2v2"/><path d="M14 12h8"/><path d="m17 20.66-1-1.73"/><path d="m17 3.34-1 1.73"/><path d="M2 12h2"/><path d="m20.66 17-1.73-1"/><path d="m20.66 7-1.73 1"/><path d="m3.34 17 1.73-1"/><path d="m3.34 7 1.73 1"/><circle cx="12" cy="12" r="2"/><circle cx="12" cy="12" r="8"/></svg>}/>
             </div>
-            <div className='flex flex-col flex-1 items-center justify-center gap-3'>
-                <span className='text-white text-5xl font-bold font-mono tracking-widest uppercase'>TELEMETRA</span>
-                <span className='mb-20 text-sm font-mono tracking-widest uppercase text-emerald-500'>Telemetry for racing enthusiasts</span>
+            <div className='flex flex-col flex-1 items-center justify-center gap-3 bg-bg-1-dark'>
+                <span className='text-text-1-dark text-5xl font-bold font-mono tracking-widest uppercase'>TELEMETRA</span>
+                <span className='mb-20 text-sm font-mono tracking-widest uppercase text-text-2-dark'>Telemetry for racing enthusiasts</span>
                 <MenuButton label="Start session" onClick={handleStartSession}/>
                 <MenuButton label="View sessions data"/>
             </div>
