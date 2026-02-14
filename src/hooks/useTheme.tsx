2a77bed76f17a557998c40153990b0414e9003f3
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-02-14 23:54:18 +0100

    Remove unused useEffect import from useTheme hook

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export function useTheme() {
     const [theme, setTheme] = useState(document.documentElement.getAttribute('data-theme') || 'light');
