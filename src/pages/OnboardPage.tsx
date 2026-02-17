e5fa63b5a663a5cb8f86b4e6705e9c08491712a7
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-02-17 09:24:18 +0100

    Better g-meter UI

--- src/pages/OnboardPage.tsx
+++ src/pages/OnboardPage.tsx
@@ -49,14 +49,14 @@ export default function OnBoardPage() {
 
                 <div className="w-full h-[50%] flex items-center justify-center bg-zinc-900/20 border-l border-zinc-900">
                     <div className="relative w-64 h-64 flex items-center justify-center">
-                        <div className="absolute w-full h bg-zinc-800"></div>
-                        <div className="absolute h-full w bg-zinc-800"></div>
+                        <div className="absolute w-full h border border-neutral-700"></div>
+                        <div className="absolute h-full w border border-neutral-700"></div>
                         
-                        <div className="absolute w-32 h-32 border border-zinc-800 rounded-full"></div>
-                        <div className="absolute w-64 h-64 border border-zinc-800 rounded-full"></div>
+                        <div className="absolute w-32 h-32 border border-neutral-400 rounded-full"></div>
+                        <div className="absolute w-64 h-64 border border-neutral-400 rounded-full"></div>
 
-                        <span className="absolute top-0 text-[10px] text-zinc-600 font-mono">LONG +</span>
-                        <span className="absolute right-0 text-[10px] text-zinc-600 font-mono">LAT +</span>
+                        <span className="absolute top-0 text-[10px] text-white font-mono">LONG</span>
+                        <span className="absolute right-0 text-[10px] text-white font-mono">LAT</span>
 
                         <div 
                             className="w-6 h-6 bg-white rounded-full shadow-[0_0_25px_rgba(255,255,255,0.8)] z-10 transition-transform duration-75 ease-out"
