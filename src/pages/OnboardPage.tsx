c0a3ee6e6bad985c90c127df96be1143e1a2b4d0
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-02-17 18:00:38 +0100

    Changed g-meter color to dinamically change when changing theme

--- src/pages/OnboardPage.tsx
+++ src/pages/OnboardPage.tsx
@@ -164,16 +164,16 @@ export default function OnBoardPage({ onCloseOnboardPage }: OnBoardPageProps) {
                     <Timer value={"1:27:38"}/>
                 </div>
 
-                <div className="w-full h-[50%] flex items-center justify-center bg-zinc-900/20 border-l border-zinc-900">
+                <div className="w-full h-[50%] flex items-center justify-center">
                     <div className="relative w-64 h-64 flex items-center justify-center pointer-events-none">
                         <div className="absolute w-full h border border-neutral-700"></div>
                         <div className="absolute h-full w border border-neutral-700"></div>
                         <div className="absolute w-32 h-32 border border-neutral-400 rounded-full"></div>
                         <div className="absolute w-64 h-64 border border-neutral-400 rounded-full"></div>
-                        <span className="absolute top-0 text-[10px] text-white font-mono">LONG</span>
-                        <span className="absolute right-0 text-[10px] text-white font-mono">LAT</span>
+                        <span className="absolute top-0 text-[10px] text-text-1 font-mono">LONG</span>
+                        <span className="absolute right-0 text-[10px] text-text-1 font-mono">LAT</span>
                         <div 
-                            className="w-6 h-6 bg-white rounded-full shadow-[0_0_25px_rgba(255,255,255,0.8)] z-10 transition-transform duration-75 ease-out"
+                            className="w-6 h-6 bg-ball rounded-full shadow-[0_0_25px_rgba(255,255,255,0.8)] z-10 transition-transform duration-75 ease-out"
                             style={{ 
                                 transform: `translate(${gForce.x}px, ${gForce.y}px)` 
                             }}
