1d5e53589e4141281b89065a91e80b55a001797c
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-02-18 16:12:57 +0100

    Fixing mistake in calibrating g-meter

--- src/pages/OnboardPage.tsx
+++ src/pages/OnboardPage.tsx
@@ -41,7 +41,7 @@ export default function OnBoardPage({ onCloseOnboardPage }: OnBoardPageProps) {
     const [isPressing, setIsPressing] = useState(false);
     const [needsPermission, setNeedsPermission] = useState(false);
     
-    const [isCalibrated, _setIsCalibrated] = useState(false);
+    const [isCalibrated, setIsCalibrated] = useState(false);
     let base_x: number;
     let base_y: number;
 
@@ -116,6 +116,7 @@ export default function OnBoardPage({ onCloseOnboardPage }: OnBoardPageProps) {
                 const { currentG } = message.payload;
                 base_x = currentG.x * 20;
                 base_y = currentG.y * 20;
+                setIsCalibrated(true);
             }
         };
 
