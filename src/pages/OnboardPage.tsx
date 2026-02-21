ec19d3718589a3c671e0f16bda1f5077aa932220
Nicola Canzonieri <nicolacanzonieri2003@gmail.com>
2026-02-21 01:03:26 +0100

    Turned on debug modal again to test Telemetra on iOS and Android devices

--- src/pages/OnboardPage.tsx
+++ src/pages/OnboardPage.tsx
@@ -52,7 +52,7 @@ export default function OnBoardPage({ startGate, finishGate, onCloseOnboardPage 
     const [lastLapTime, setLastLapTime] = useState(0);
     
     // DEBUG PURPOSE ONLY
-    const [showDebug, _setShowDebug] = useState(false);
+    const [showDebug, _setShowDebug] = useState(true);
     const [posLan, setPosLan] = useState<number>();
     const [posLng, setPosLng] = useState<number>();
     
